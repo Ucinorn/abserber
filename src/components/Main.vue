94fb0a1fc2d0382f697cc3e1187f1b6fcf6c45be
Tom Muller <thomasamuller@gmail.com>
2023-04-04 09:56:23 +1000

    changes

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,25 +1,39 @@
 <template>
-  <div class="w-100 fill-height">
+  <div class="w-100 fill-height" style="font-size: 90%; font-family: monospace">
     <v-container>
-      <!-- BATTLE AREA, PLAYER AND ENEMIES -->
-      <div class="d-flex" id="battle">
-        <div class="mr-auto">
-          <Player />
-        </div>
-        <div class="ml-2" v-for="(enemy, index) in battle" :key="`enemy_${index}`" >
-          <div style="max-width: 4rem">
-            <Enemy :enemy="enemy" :active="true"/>
+              <!-- BATTLE AREA AND ENEMIES -->
+        <div class="d-flex min-h-200 mb-4">
+          <div class="mr-2" v-for="(enemy, index) in battle" :key="`enemy_${index}`" >
+            <div style="max-width: 4rem">
+              <Enemy :enemy="enemy" :active="true"/>
+            </div>
+          </div>
+          <div class="ml-auto w-25">
+            <transition name="fade-right">
+              <div v-show="enemySelected">
+                <Enemy v-if="enemySelected" :enemy="selectedEnemy" :detailed="true"/>
+              </div>
+            </transition>
           </div>
         </div>
-      </div>
-      <!-- ENEMY SELECT -->
-      <div class="my-4">
-        <div v-for="enemies in [ basic, intermediate ]" class=" pa-2 mb-2">
-          <v-row>
-            <v-col v-for="( enemy, key ) in enemies" :key="key" cols="3">
-              <Enemy :enemy="enemy" @add="addEnemy(enemy)"/>
-            </v-col>
-          </v-row>
+      <div class="d-flex">
+        <div class="w-25 pr-2">
+          <!-- PLAYER SIDEBAR -->
+          <Player />
+        </div>
+        <!-- ENEMY SELECT -->
+        <div class="mb-4 px-2">
+          <div v-for="enemies in [ basic, intermediate ]" class="mb-2">
+            <v-row>
+              <v-col v-for="( enemy, key ) in enemies" :key="key" cols="3">
+                <Enemy
+                  :enemy="enemy"
+                  :selected="( selectedEnemy && selectedEnemy.id == enemy.id )"
+                  @click="selectedEnemy = enemy"
+                  @dblclick="addEnemy(enemy)"/>
+              </v-col>
+            </v-row>
+          </div>
         </div>
 
       </div>
@@ -28,7 +42,7 @@
 </template>
 
 <script setup lang="ts">
-  import { computed } from 'vue'
+  import { computed, ref } from 'vue'
 
   import Player from '@/components/Player.vue'
   import Enemy from '@/components/Enemy.vue'
@@ -38,7 +52,11 @@
   import { player, constants } from '@/js/store.js'
 
   const { basic, intermediate } = enemies
+
   const battle = []
+  const selectedEnemy = ref({})
+
+  const enemySelected = computed( () => !!('name' in selectedEnemy.value) )
   const canAddEnemy = computed( ( ) => battle.length >= constants.maxEnemies )
   const addEnemy = ( e ) => {
     if ( ! canAddEnemy ) return
@@ -49,3 +67,41 @@
     battle.push( { ...e })
   }
 </script>
+
+<style>
+.min-h-50 {
+  min-height:50px
+}
+.min-h-100 {
+  min-height:100px
+}
+.min-h-200 {
+  min-height:200px
+}
+.fade-right-enter-active {
+  animation: fade-right-in 0.5s;
+}
+.fade-right-leave-active {
+  animation: fade-right-out 0.5s;
+}
+@keyframes fade-right-in {
+  from {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+@keyframes fade-right-out {
+  from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+}
+</style>
